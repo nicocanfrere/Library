112e7cbb5a969784c64c26bb7de20eff2d21f6cc
Nicolas Canfrere <nico.canfrere@gmail.com>
2021-10-31 20:58:32 +0100

    doc : php code sniffer

--- src/App/Handler/ApiDocHandler.php
+++ src/App/Handler/ApiDocHandler.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Handler;
 
 use Laminas\Diactoros\Response\JsonResponse;
@@ -7,15 +9,15 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-/**
- * Class ApiDocHandler
- */
+use function file_get_contents;
+use function json_decode;
+
 class ApiDocHandler implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $content = file_get_contents(__DIR__ . '/../../../data/api-doc/openapi.json');
 
-        return new JsonResponse(\json_decode($content));
+        return new JsonResponse(json_decode($content));
     }
 }
--- src/App/Handler/IndexHandlerFactory.php
+++ src/App/Handler/IndexHandlerFactory.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
-use Mezzio\Helper\UrlHelper;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-
-use function var_dump;
 
 class IndexHandlerFactory
 {
