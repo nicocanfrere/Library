28077d90652e9ac5e1a54b34b46ff24bee6b2640
Nicolas Canfrere <nico.canfrere@gmail.com>
2021-11-01 08:40:48 +0100

    fix : create log file

--- src/Infrastructure/Logger/StreamHandlerFactory.php
+++ src/Infrastructure/Logger/StreamHandlerFactory.php
@@ -7,6 +7,11 @@ namespace Infrastructure\Logger;
 use Monolog\Handler\HandlerInterface;
 use Monolog\Handler\StreamHandler;
 use Psr\Container\ContainerInterface;
+use RuntimeException;
+
+use function chmod;
+use function is_file;
+use function touch;
 
 class StreamHandlerFactory
 {
@@ -16,6 +21,14 @@ class StreamHandlerFactory
             ? $container->get('config')
             : [];
         $config = $config['logger']['handlers']['stream_handler'] ?? [];
+
+        if (empty($config['path'])) {
+            throw new RuntimeException('Log file path is not defined!');
+        }
+        if (! is_file($config['path'])) {
+            touch($config['path']);
+            chmod($config['path'], 0777);
+        }
 
         return new StreamHandler(
             $config['path']
